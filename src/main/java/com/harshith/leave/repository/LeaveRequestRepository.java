1cddfde319153e9a1c79a54cf0eb833f053c6ffa
M SAI HARSHITH GOUD <166594237+thenameisharshith@users.noreply.github.com>
2026-09-18 11:00:58 +0530

    Add Employee Leave Management System project

---
+++ src/main/java/com/harshith/leave/repository/LeaveRequestRepository.java
@@ -0,0 +1,9 @@
+package com.harshith.leave.repository;
+
+import com.harshith.leave.entity.LeaveRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
+    List<LeaveRequest> findByEmployeeId(Long employeeId);
+}
