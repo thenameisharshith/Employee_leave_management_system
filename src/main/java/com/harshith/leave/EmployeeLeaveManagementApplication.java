85f675dca23832b8268f6455df59aa3df6d6b173
M SAI HARSHITH GOUD <166594237+thenameisharshith@users.noreply.github.com>
2026-09-18 11:00:27 +0530

    Add Employee Leave Management System project

---
+++ src/main/java/com/harshith/leave/EmployeeLeaveManagementApplication.java
@@ -0,0 +1,11 @@
+package com.harshith.leave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLeaveManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeLeaveManagementApplication.class, args);
+    }
+}
